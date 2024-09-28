cc18475c3e12310c0f6b63583fadafdabb04fc11
M.Rizal <rizandi778@gmail.com>
2024-09-28 08:55:55 +0700

    Main sama Bunga Edelweis

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author rizan
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        Bunga_Edelweis fr = new Bunga_Edelweis();
+
+        fr.setnama_ilmiah("Leontopolium alpinum");
+        fr.setWarna("Putih atau keperakan ditengahnya kuning");
+        fr.setIklim("Sejuk hingga dingin");
+        fr.setTanah("Berbatu");
+        fr.setHewan_penyerbuk("Lebah dan kupu-kupu");
+
+        System.out.println("\n\t\t\tBunga Edelweis");
+        System.out.println("Nama Ilmiah       : " + fr.getnama_ilmiah());
+        System.out.println("Warna             : " + fr.getWarna());
+        System.out.println("Iklim             : " + fr.getIklim());
+        System.out.println("Tanah             : " + fr.getTanah());
+        System.out.println("Hewan Penyerbuk   : " + fr.getHewan_penyerbuk());
+    }
+}
